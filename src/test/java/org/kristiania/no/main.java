3f96c49fc68c628f1aedf93f7ad16f0a75d864d5
larshjelen <larshjelen@gmail.com>
2020-09-08 20:18:59 +0200

    wsedrt

--- src/test/java/org/kristiania/no/main.java
+++ src/test/java/org/kristiania/no/main.java
@@ -5,7 +5,7 @@ public class main {
         main main = new main();
 
         public static String toRoman(int number) {
-            String[] thousands = {"", "M", "MM"};
+            String[] thousands = {"", "M", "MM", "MMM"};
             String[] hundreds = {"","C","CC","CCC","CD", "D", "DC", "DCC", "DCCC", "CD" };
             String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
             String[] units = {"","I","II","III","IV","V", "VI", "VII", "VIII", "IX"};
